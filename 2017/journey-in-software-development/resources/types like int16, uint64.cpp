434427904cba9ece6d0474bfb330eccc66cdcc9a
Dmitri Shuralyov <shurcooL@gmail.com>
2017-03-20 17:27:32 -0400

    Add "A journey in software development" guest talk at UOIT on 2017-03-17.
    A guest talk I gave at a second year CSE class at University of Ontario
    Institute of Technology (UOIT) on March 17, 2017.
    
    It's about Go (a quick introduction), and highlights of interesting
    things I've done and seen while working on my master's degree,
    at startups, on personal projects, and in open source.
    
    A video of this talk was recorded and can be seen at
    https://www.youtube.com/watch?v=XSd9OAHVXMI.

---
+++ 2017/journey-in-software-development/resources/types like int16, uint64.cpp
@@ -0,0 +1,19 @@
+#ifdef WIN32
+	typedef signed __int8       int8;
+	typedef signed __int16      int16;
+	typedef signed __int32      int32;
+	typedef signed __int64      int64;
+	typedef unsigned __int8     uint8;
+	typedef unsigned __int16    uint16;
+	typedef unsigned __int32    uint32;
+	typedef unsigned __int64    uint64;
+#elif (defined(__APPLE__) && defined(__MACH__)) || defined(__linux)
+	typedef int8_t              int8;
+	typedef int16_t             int16;
+	typedef int32_t             int32;
+	typedef int64_t             int64;
+	typedef uint8_t             uint8;
+	typedef uint16_t            uint16;
+	typedef uint32_t            uint32;
+	typedef uint64_t            uint64;
+#endif // WIN32
